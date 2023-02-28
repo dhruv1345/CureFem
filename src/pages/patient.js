35e371e2d0bcf21cf959c5ad042186a9297101c5
dhruv1345 <21102156@mail.jiit.ac.in>
2023-02-28 11:27:02 +0530

    Upated react components

---
+++ src/pages/patient.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Patient(){
+    return (
+        <div className="page1">
+            <h3 className='headings'>Patient</h3><br />
+            <img className='imagesPage1' src="patient.jpg" alt="" width='350' height='250' /><br /><br />
+            <h3 className='captioning'>If you are a Patient. Click Image.</h3>
+        </div>
+    );
+}
+
+export default Patient;
