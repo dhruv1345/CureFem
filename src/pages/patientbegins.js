132545d238e08ab1da0e09444703cf437c3987a6
dhruv1345 <21102156@mail.jiit.ac.in>
2023-02-28 23:56:07 +0530

    making pateint page

---
+++ src/pages/patientbegins.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './CSS/patientBegin.css';
+
+function Register(){
+    return(
+        <div className="wrappingPatient">
+            <div className="leftSideImage">
+                <h1>Namaste</h1>
+            </div>
+            <div className="rightSideContent">
+                <h1>Patient</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
