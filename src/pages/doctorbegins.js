6c7e43f853ae1f2aaf5c2bd9b55b4f721eaaa5dd
dhruv1345 <21102156@mail.jiit.ac.in>
2023-03-03 22:39:53 +0530

    Made doctors window.

--- src/pages/doctorbegins.js
+++ src/pages/doctorbegins.js
@@ -12,6 +12,24 @@ function Doctory(){
             <div className="contentdoc">
                 <h2>Be the Dr. who can change the lives of Women</h2>
             </div>
+
+            <div className="secondsection">
+                <div className="imaging">
+                    <img className="imagation" src="thanks.jpg" alt="" />
+                    <div className="h4AndSymbo">
+                        <h4 className="h4image">Here's how you can help <br /> needy <span>Women</span></h4>
+                        <p className="pimage">
+                        <ul>
+                            <li className="list">You can give live Consultancies.</li>
+                            <li className="list">We can provide you platform to contact affected Women.</li>
+                            <li className="list">You can help women by providing free Doctor Sessions.</li>
+                        </ul>
+                        </p>
+                        <button className="button1">Click to See their Problems</button>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
 
     );
